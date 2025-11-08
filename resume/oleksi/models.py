0d4984fcdf96b53faf37ec10865319c36aae72db
VerloxPy <oleksijkuracij38@gmail.com>
2025-11-09 01:56:35 +0200

    Delete resume directory

--- resume/oleksi/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Comment(models.Model):
-    comment_text = models.CharField(max_length=200, verbose_name="Коментар")
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата створення")
